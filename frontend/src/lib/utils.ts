a46a89b8dbbea63f4358fbae309608e9b4498065
Ethan Ford <ethanfordj5@gmail.com>
2026-01-02 10:29:48 -0700

    :bug: got rid of unneded import

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import type { MediaComponentProps } from "@/validation/valSchema";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
